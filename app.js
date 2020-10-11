b304489a117a4d082ea3fd12c115b727bf655c62
ninel <zabaykinanm@mer.ci.nsu.ru>
2020-10-11 12:14:24 +0700

    added method render screen

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const URL = 'http://localhost:3000';
 class App extends React.Component {
+
     constructor() {
         super();
+        // эти переменные будут меняться динамически
         this.state = {
             nick: '',
             message: '',
             serverMessages: []
         };
+
+        // получение новых сообщений в цикле
+        //я делаю bind, чтобы у функции был определён контекст this
         setInterval(this.getMessages.bind(this), 1000);
-        }
     }
-        postMessage()
-    {
+
+    postMessage() {
         // метод отправки сообщения
         let xhr = new XMLHttpRequest();
         xhr.open('POST', URL);
@@ -31,32 +35,58 @@ class App extends React.Component {
         xhr.onerror = function() {
             console.log('Запрос не удался');
         };
-    };
+    }
 
-    getMessages()
-    {
+    getMessages() {
         // метод получения сообщений
         let xhr = new XMLHttpRequest();
         xhr.open('GET', URL);
         xhr.send();
         xhr.onload = () => {
-            if (xhr.status != 200) {
+            if (xhr.status !== 200) {
                 console.error('Ошибка!');
             } else {
                 this.drawMessages(xhr.response);
             }
         };
-    };
+    }
 
-    drawMessages(response)
-    {
+    drawMessages(response) {
         // метод отрисовки сообщений
         const newServerMessages = JSON.parse(response);
-        const existingIds = this.serverMessages.map(message => message.id);
-        for (let serverMessage of newServerMessages) {
-            if (!existingIds.includes(serverMessage.id)) {
-                this.messages.innerHTML += `<ul><b>${serverMessage.nick}:</b> ${serverMessage.message}</ul>`;
-            }
-        }
-        this.serverMessages = newServerMessages;
-    };
+        this.setState({serverMessages: newServerMessages});
+    }
+
+    render() {
+        return <>
+            <h1>Чат</h1>
+            <form>
+                <input
+                    value={this.state.nick}
+                    type="text"
+                    onChange={e => this.setState({nick: e.target.value})}
+                />
+                <br/>
+                <textarea
+                    value={this.state.message}
+                    onChange={e => this.setState({message: e.target.value})}
+                >
+                </textarea>
+                <br/>
+                <input
+                    type="button"
+                    value="отправить"
+                    onClick={() => this.postMessage()}
+                />
+            </form>
+            <ul>
+                {this.state.serverMessages.map((message, index) => (
+                    <li key={index}>
+                        <b>{message.nick}:</b>
+                        {message.message}
+                    </li>
+                ))}
+            </ul>
+        </>
+    }
+}
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const app = new App();
 });
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
