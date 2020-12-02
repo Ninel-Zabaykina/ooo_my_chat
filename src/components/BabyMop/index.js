35b4ec0e931de18d6db9bda61900e1045a803c5c
ninel <zabaykinanm@mer.ci.nsu.ru>
2020-12-02 23:57:13 +0700

    styles

--- src/components/BabyMop/index.js
+++ src/components/BabyMop/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import cat from './Baby-Mop1.jpg';
+import baby from './Baby-Mop1.jpg';
+import styles from './styles.css';
 
 export default () => {
-    return <img src={baby} />;
+    return <img src={baby} className="baby-picture" />;
 };
