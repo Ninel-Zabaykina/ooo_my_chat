f6de3e2f1d08b3be1d39ce3dfb2965d78c9a14c4
ninel <zabaykinanm@mer.ci.nsu.ru>
2020-12-03 20:24:02 +0700

    snapshot

--- src/components/BabyMop/index.js
+++ src/components/BabyMop/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import baby from './Baby-Mop1.jpg';
-import styles from './styles.css';
+import './styles.css';
 
 export default () => {
     return <img src={baby} className="baby-picture" />;
--- src/components/Message.test.js
+++ src/components/Message.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
 import Message from './Message';
+
+describe('Message component', () => {
+
+    test('Message matches snapshot', () => {
+        const nick = 'test';
+        const message = 'test';
+        const component = renderer.create(<Message nick={nick} message={message}/>).toJSON();
+        expect(component).toMatchSnapshot();
+    });
+
     test('Message shows nick and message', () => {
         const nick = 'test';
         const message = 'test';
@@ -8,3 +19,4 @@ import Message from './Message';
         expect(component.text()).toContain(nick);
         expect(component.text()).toContain(message);
     });
+});
