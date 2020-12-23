38115d944bcdb7f261de116d80673afe4792f0f4
ninel <zabaykinanm@mer.ci.nsu.ru>
2020-12-23 23:06:25 +0700

    fixed chatlist

--- src/stories/ChatList.stories.js
+++ src/stories/ChatList.stories.js
@@ -13,15 +13,31 @@ export const Primary = Template.bind({});
 Primary.args = {
     list: [
         {
-            id: '123',
-            createdAt: 'date1',
-            title: 'My chat'
+            id: '001',
+            createdAt: 'Dec 2020',
+            title: 'My chat',
+            userId: '001',
+            participants: ['001', '002'],
+            isPrivate: false
         },
         {
-            id: '456',
-            createdAt: 'date2',
-            title: 'My chat too'
+            id: '002',
+            createdAt: 'Nov 2020',
+            title: 'My chat too',
+            userId: '002',
+            participants: ['001', '002'],
+            isPrivate: false
+        },
+        {
+            id: '003',
+            createdAt: 'Oct 2020',
+            title: 'Я не знаю это чьё',
+            userId: '003',
+            participants: ['002', '003'],
+            isPrivate: false
         }
     ],
-    clickHandle: action('clicked!')
+    goHandle: action('go'),
+    deleteHandle: action('delete'),
+    joinHandle: action('join')
 };
